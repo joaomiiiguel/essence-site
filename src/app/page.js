7745c69ea3ffff754487acd2bfe7ede293c89a4a
João Miguel <macbookprom@Macbooks-MacBook-Pro.local>
2023-06-06 12:29:04 -0300

    tester page

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,21 @@
-'use client'
-import AboutComp from "../components/home/AboutComp"
-import BannerCover from "../components/home/BannerCover"
-import BoxContatWhat from "../components/home/BoxContatWhat"
-import ListClients from "../components/home/ListClients"
-import PortifolioComp from "../components/home/PortifolioComp"
-import ServiceComp from "../components/home/ServiceComp"
+// import AboutComp from "../components/home/AboutComp"
+// import BannerCover from "../components/home/BannerCover"
+// import BoxContatWhat from "../components/home/BoxContatWhat"
+// import ListClients from "../components/home/ListClients"
+// import PortifolioComp from "../components/home/PortifolioComp"
+// import ServiceComp from "../components/home/ServiceComp"
 
 export default function Home() {
 
   return (
-    <main className="w-full min-h-screen text-white ">
-      <BannerCover />
+    <main className="w-full min-h-screen text-black flex justify-center items-center">
+      <p>Miguel</p>
+      {/* <BannerCover />
       <AboutComp />
       <ServiceComp />
       <PortifolioComp />
       <ListClients />
-      <BoxContatWhat />
+      <BoxContatWhat /> */}
     </main>
   )
 }
